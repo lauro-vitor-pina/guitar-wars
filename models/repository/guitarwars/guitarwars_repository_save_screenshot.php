cc937236ef96e657cb672add44b91c84e4009c0c
lauro-vitor-pina <lauro.pina@outlook.com>
2025-04-28 21:23:28 -0300

    Create directory image if not exists

--- models/repository/guitarwars/guitarwars_repository_save_screenshot.php
+++ models/repository/guitarwars/guitarwars_repository_save_screenshot.php
@@ -4,9 +4,15 @@ require_once(__DIR__ . '../../../../appvars.php');
 
 function guitarwars_repository_save_screenshot($command_view_model)
 {
-    $target = __DIR__  . '../../../../' . GW_IMAGE_PATH .$command_view_model['screenshot'];
+    $target_directory = __DIR__  . '../../../../' . GW_IMAGE_PATH;
 
-    $result_move = move_uploaded_file($command_view_model['screenshot_tmp_name'], $target);
+    if (!is_dir($target_directory)) {
+        mkdir($target_directory);
+    }
+
+    $to = $target_directory . $command_view_model['screenshot'];
+
+    $result_move = move_uploaded_file($command_view_model['screenshot_tmp_name'], $to);
 
     return $result_move;
 }
