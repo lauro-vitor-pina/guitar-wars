5d62fe6e74eae4ab244a2b2e455e9483bb1747a1
lauro-vitor-pina <lauro.pina@outlook.com>
2025-04-28 21:34:41 -0300

    add permision for creating folder on server

--- models/repository/guitarwars/guitarwars_repository_save_screenshot.php
+++ models/repository/guitarwars/guitarwars_repository_save_screenshot.php
@@ -7,7 +7,7 @@ function guitarwars_repository_save_screenshot($command_view_model)
     $target_directory = __DIR__  . '../../../../' . GW_IMAGE_PATH;
 
     if (!is_dir($target_directory)) {
-        mkdir($target_directory);
+        mkdir($target_directory, 0777, true);
     }
 
     $to = $target_directory . $command_view_model['screenshot'];
